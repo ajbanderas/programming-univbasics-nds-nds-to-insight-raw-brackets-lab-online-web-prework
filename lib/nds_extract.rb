d2a67e092eb9b9e8c5ff42b358ed52b141e50c46
Kevin Downs <kevin.downs04@gmail.com>
2019-10-24 19:44:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,5 +5,5 @@ def directors_totals(nds)
   directors = directors_database
   result = {}
   total = 0
-  puts directors[0][:movies][:worldwide_gross]
+  puts directors[0][:movies][0][:worldwide_gross]
 end
